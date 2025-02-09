8fe47f071067da62f8923a9a47c43aceb0abbb4b
Dev_Skoll <me@skoll.dev>
2025-02-08 22:49:40 -0500

    Started Dashboard app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'dashboard'
+
 urlpatterns = [
-    path('', views.DashboardView, name='dashboard-home'),
+    path('', views.DashboardView, name='home'),
 ]
